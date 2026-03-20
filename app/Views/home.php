f3945ee56a4b089d7a463e2a844ac8482849b0d6
Philip Newborough <corenominal@corenominal.org>
2026-03-20 21:16:46 +0000

    Remove close button from toast component in home view

--- app/Views/home.php
+++ app/Views/home.php
@@ -408,8 +408,6 @@ code block
         aria-live="assertive" aria-atomic="true">
         <div class="d-flex">
             <div class="toast-body" id="app-toast-body"></div>
-            <button type="button" class="btn-close btn-close-white me-2 m-auto"
-                data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
     </div>
 </div>
